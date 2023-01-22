7ad6109ee70197cf776701d1df700abc1ccb9fb8
V.Nearonsky <v.nearonsky@yandex.ru>
2023-01-22 18:19:19 +0300

    Решение первойй задачи

---
+++ task8/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом. Не используйте строки.
+14212 -> нет
+12821 -> да
+23432 -> да*/ 
+
+int InputInt(string message)
+{
+    Console.Write(message + " > ");
+    string inputValue = Console.ReadLine()!;
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+int number  = InputInt("Введите число, чтобы узнать является ли оно палиндромом: ");
+int number2 = 0;
+int check = number;
+
+while (number != 0)
+{
+   int intermediateNumber = number % 10;
+   number = number / 10;
+   number2 = number2 * 10 + intermediateNumber;
+}
+if(number2 == check)
+{
+   Console.WriteLine($"{check} - палиндром");
+}
+else 
+{
+   Console.WriteLine($"{check} - не палиндром");
+}
+
+
+
+
+
