10d55e0c7a8aea086e8764e232d0948627292b67
V.Nearonsky <v.nearonsky@yandex.ru>
2023-01-22 20:28:56 +0300

    Решение второй задачи.

--- task9/Program.cs
+++ task9/Program.cs
@@ -15,9 +15,7 @@ double GetLength(double x1, double y1, double z1, double x2, double y2, double z
 {
    return Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) +Math.Pow(z1-z2, 2));
 }  
-  // double length = GetLength(x1, y1, z1, x2, y2, z2);
- //  System.Console.WriteLine(length);
-
+ 
 Console.WriteLine("Введите координаты первой точки: ");
 double x1 = InputInt("x1 = ");
 double y1 = InputInt("y1 = ");
