833878a3a923d911d5fdf0b85c4b522a0996942f
V.Nearonsky <v.nearonsky@yandex.ru>
2023-01-23 23:32:55 +0300

    Решение дополнительной задачи

---
+++ task11/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 5*: Напишите программу, которая генерирует последовательность случайных чисел из 10 
+// элементов в диапазоне от 1 до 10, и подсчитывает, сколько сгенерировалось чисел больше 5.
+
+void FillArray(int[] collection)
+{
+   int index = 0;
+   int length = collection.Length;
+   while (index < length)
+   {
+      collection[index] = new Random().Next(1, 10);
+      Console.WriteLine(collection[index]);
+      index++;
+   }
+}
+
+int CountArray(int[] col)
+{
+   int number = 0;
+   int position = 0;
+   int count = col.Length;
+   while(position < count)
+   {
+      if(col[position] > 5)
+      {
+         number++;
+      }
+   position++;
+   } 
+   return number;
+}
+
+int[] array = new int[10];
+FillArray(array);
+Console.WriteLine ($"Количество чисел > 5 = {CountArray(array)}");
+
+
+
+
+   
