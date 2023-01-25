10b135637d0b71fb73e39121315f668dee979cfd
V.Nearonsky <v.nearonsky@yandex.ru>
2023-01-26 00:45:28 +0300

    Решение первой задачи.

---
+++ task12/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 6*: Напишите программу, которая выводит первые заданные элемента следующей последовательности:
+// -> 12
+// 1 2 2 3 3 3 4 4 4 4 5 5
+
+void FillArray(int[] numbers)
+{
+   int length = numbers.Length;
+   int index = 1;
+   int count = 1;
+   int position = 0;
+   while(position < length)
+   {
+      //int position = 0;
+      while(position < index)
+      {
+         numbers[index] = count;
+         System.Console.WriteLine(numbers[index]);
+         position++;
+         //index++;
+         //if(position > 12)
+         {
+         //break;
+         }
+      } 
+      count++;
+      index = index + count;
+      //position++;     
+   }
+   //count++;   
+}
+int[] array = new int[12];
+FillArray(array);
---
+++ task13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в 
+// натуральную степень B. Не использовать Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int InputInt(string message)
+{
+   System.Console.Write(message + " -> ");
+   string inputValue = Console.ReadLine()!;
+   int result = Convert.ToInt32(inputValue);
+   return result;
+}
+
+int number = InputInt("Введите число");
+int degree = InputInt("Введите степень, в которую надо возвести число");
+
+int Exponentiation(int degree)
+{
+   int resultat = 1;   
+   for(int i = 0; i < degree; i++)
+   {
+      resultat = resultat * number;
+   }
+   return resultat;
+}
+
+int resultExponent = Exponentiation(degree);
+System.Console.WriteLine($"{number} в степени {degree} = {resultExponent}");
