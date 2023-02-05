ace54a3c2851d340cb1d14855a214790b70d84f4
V.Nearonsky <v.nearonsky@yandex.ru>
2023-02-05 23:36:10 +0300

    Решение первой задачи.

---
+++ task19/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int InputInt(string message)
+{
+   Console.Write(message + " -> ");
+   return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] FillArray(int size)
+{
+   int[] array = new int[size];
+   for (int i = 0; i < array.Length; i++)
+   {
+      array[i] = int.Parse(Console.ReadLine());
+   }
+   return array;
+}
+
+int CountPositiveNumber(int[] array)
+{
+   int count = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if(array[i] > 0)
+      {
+         count++;
+      }
+   }
+   return count;
+}
+
+int size = InputInt("Введите количество элементов массива");
+System.Console.WriteLine("Введите элемент массива, после ввода нажмите ENTER:");
+int[] array = FillArray(size);
+Console.WriteLine($"Положительных чисел в массиве -> {CountPositiveNumber(array)}");
---
+++ task20/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int InputInt(string message)
+{
+   Console.Write(message + " -> ");
+   return Convert.ToInt32(Console.ReadLine());
+}
+
+void CheckCoordinates(double k1, double k2)
+{
+   if(k1 == k2)
+   {
+      Console.WriteLine("Прямые не пересекутся");
+   }
+}
+
+double CoordinatesX(double b1, double b2, double k1,double k2)
+{
+   double x = (b2 - b1) / (k1 - k2);
+   return x;
+}
+
+double CoordinatesY(double b1, double k1, double x)
+{
+   double y = k1 * x + b1;
+   return y;
+}
+
+double b1 = InputInt("Введите значение b1");
+double k1 = InputInt("Введите значение k1");
+double b2 = InputInt("Введите значение b2");
+double k2 = InputInt("Введите значение k2");
+CheckCoordinates(k1, k2);
+double x = CoordinatesX(b1, b2, k1, k2);
+double y = CoordinatesY(b1, k1, x);
+System.Console.WriteLine();
+System.Console.WriteLine($"Прямые пересекутся в точке ({x}; {y})");
